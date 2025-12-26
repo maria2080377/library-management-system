858b1865d69d26b7f5e98defe05235bc11681b79
maria2080377 <masha900p@gmail.com>
2025-12-26 14:58:26 +0300

    Add Main class skeleton

---
+++ src/main/java/library/Main.java
@@ -0,0 +1,16 @@
+package library;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем библиотеку
+        Library library = new Library();
+
+        // TODO: добавить книги и продемонстрировать функциональность
+        // Код демонстрации будет добавлен в следующем коммите
+
+        System.out.println("Библиотечная система запущена.");
+        System.out.println("Демонстрационный код будет добавлен в следующем коммите.");
+    }
+}
